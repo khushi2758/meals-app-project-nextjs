20f7f34c2b85db141cf793fae80cafb6eec2bbbf
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2025-07-14 03:43:46 +0530

    image page complete

--- component/main-header.js
+++ component/main-header.js
@@ -1,8 +1,29 @@
 import Link from 'next/link';
+import logoImg from "@/assets/logo.png";
+import classes from './main-header.module.css';
+import Image from 'next/image';
 export default function MainHeader() {
   return (
-    <header>
-      <Link  href="/"></Link>
+    <header className={classes.header}>
+      <Link className={classes.logo}   href="/">
+      <Image src={logoImg} alt='a plate with food on it' priority/>
+      nextLevel Food
+      </Link>
+
+      <nav className={classes.nav} >
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+           <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>     
+             
+        </ul>
+
+      
+          
+      </nav>
     </header>       
   );
 }
