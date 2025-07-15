c7dc2d5d68c7923d8a136765aeba536048114b4e
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2025-07-15 20:06:18 +0530

    update

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,28 @@
-export default function MealsPage() {
+import classes from './page.module.css';
+import MealsGrid from '@/component/meals/meals-grid';
+import { getMeals} from '@/lib/meals';  
+import Link from 'next/link';
+export default async function MealsPage() {
+  const meals = await getMeals();
+  // This function fetches meals from the database and passes them to the MealsGrid component
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Meals Page - Coming Soon!
-      </h1>
+  <>
+  <header className = {classes.header} >
+    <h1>
+      Delicious meats, created {' '}
+       <span className={classes.highlight}> by you!</span>
+    </h1>
+    <p>choose your favourite recipe and cook it yourself. It is easy and fun.</p>
+    <p className={classes.cta }>
+      <Link href="/meals/share">
+      Share your favourite recipe
+      </Link>
+    </p>
+    <main classname = {classes.main} >
+      <MealsGrid meals={meals} />
     </main>
+  </header>
+  
+  </>
   );
 }
